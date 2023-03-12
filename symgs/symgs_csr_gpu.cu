11728b282c48fbda43d60e89a4ad2251427c37ae
callegarimattia <mattia.callegari@mail.polimi.it>
2023-03-13 00:42:19 +0100

    Initial commit, .cu file added

---
+++ symgs/symgs_csr_gpu.cu
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <sys/time.h>
+
+double get_time() // function to get the time of day in seconds
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec * 1e-6;
+}
+
+// Reads a sparse matrix and represents it using CSR (Compressed Sparse Row) format
+void read_matrix(int **row_ptr, int **col_ind, float **values, float **matrixDiagonal, const char *filename, int *num_rows, int *num_cols, int *num_vals)
+{
+    int err;
+    int *row_ptr_t, *col_ind_t;
+    float *values_t, *matrixDiagonal_t;
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        fprintf(stdout, "File cannot be opened!\n");
+        exit(0);
+    }
+    // Get number of rows, columns, and non-zero values
+    if(fscanf(file, "%d %d %d\n", num_rows, num_cols, num_vals)==EOF)
+        printf("Error reading file");
+
+    cudaMallocManaged(&row_ptr_t, (*num_rows + 1) * sizeof(int));
+    cudaMallocManaged(&col_ind_t, *num_vals * sizeof(int));
+    cudaMallocManaged(&values_t, *num_vals * sizeof(float));
+    cudaMallocManaged(&matrixDiagonal_t, *num_rows * sizeof(float));
+    
+    // Collect occurances of each row for determining the indices of row_ptr
+    int *row_occurances = (int *)malloc(*num_rows * sizeof(int));
+    for (int i = 0; i < *num_rows; i++)
+    {
+        row_occurances[i] = 0;
+    }
+
+    int row, column;
+    float value;
+    while (fscanf(file, "%d %d %f\n", &row, &column, &value) != EOF)
+    {
+        // Subtract 1 from row and column indices to match C format
+        row--;
+        column--;
+        row_occurances[row]++;
+    }
+
+    // Set row_ptr
+    int index = 0;
+    for (int i = 0; i < *num_rows; i++)
+    {
+        row_ptr_t[i] = index;
+        index += row_occurances[i];
+    }
+    row_ptr_t[*num_rows] = *num_vals;
+    free(row_occurances);
+
+    // Set the file position to the beginning of the file
+    rewind(file);
+
+    // Read the file again, save column indices and values
+    for (int i = 0; i < *num_vals; i++)
+    {
+        col_ind_t[i] = -1;
+    }
+
+    if(fscanf(file, "%d %d %d\n", num_rows, num_cols, num_vals)==EOF)
+        printf("Error reading file");
+    
+    int i = 0, j = 0;
+    while (fscanf(file, "%d %d %f\n", &row, &column, &value) != EOF)
+    {
+        row--;
+        column--;
+
+        // Find the correct index (i + row_ptr_t[row]) using both row information and an index i
+        while (col_ind_t[i + row_ptr_t[row]] != -1)
+        {
+            i++;
+        }
+        col_ind_t[i + row_ptr_t[row]] = column;
+        values_t[i + row_ptr_t[row]] = value;
+        if (row == column)
+        {
+            matrixDiagonal_t[j] = value;
+            j++;
+        }
+        i = 0;
+    }
+    fclose(file);
+    *row_ptr = row_ptr_t;
+    *col_ind = col_ind_t;
+    *values = values_t;
+    *matrixDiagonal = matrixDiagonal_t;
+}
+
+// CPU implementation of SYMGS using CSR, DO NOT CHANGE THIS
+void symgs_csr_sw(const int *row_ptr, const int *col_ind, const float *values, const int num_rows, float *x, float *matrixDiagonal)
+{
+
+    // forward sweep
+    for (int i = 0; i < num_rows; i++)
+    {
+        float sum = x[i];
+        const int row_start = row_ptr[i];
+        const int row_end = row_ptr[i + 1];
+        float currentDiagonal = matrixDiagonal[i]; // Current diagonal value
+
+        for (int j = row_start; j < row_end; j++)
+        {
+            sum -= values[j] * x[col_ind[j]];
+        }
+
+        sum += x[i] * currentDiagonal; // Remove diagonal contribution from previous loop
+
+        x[i] = sum / currentDiagonal;
+    }
+
+    // backward sweep
+    for (int i = num_rows - 1; i >= 0; i--)
+    {
+        float sum = x[i];
+        const int row_start = row_ptr[i];
+        const int row_end = row_ptr[i + 1];
+        float currentDiagonal = matrixDiagonal[i]; // Current diagonal value
+
+        for (int j = row_start; j < row_end; j++)
+        {
+            sum -= values[j] * x[col_ind[j]];
+        }
+        sum += x[i] * currentDiagonal; // Remove diagonal contribution from previous loop
+
+        x[i] = sum / currentDiagonal;
+    }
+}
+
+//__global__ 
+void parallel_symgs_csr_sw(const int *row_ptr, const int *col_ind, const float *values, const int num_rows, float *x, float *matrixDiagonal){
+    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+    for (size_t i = thread_id; i < num_rows; i++)
+    {
+        float sum = x[i];
+        const int row_start = row_ptr[i];
+        const int row_end = row_ptr[i + 1];
+        float currentDiagonal = matrixDiagonal[i]; // Current diagonal value
+
+        
+    }
+    
+}
+
+bool isResultValid(float *x_cpu, float *x_gpu, long size){
+    for (size_t i = 0; i < size; i++)
+    {
+        if (x_cpu[i] != x_gpu[i])
+            return false;
+    }
+    return true;
+}
+
+int main(int argc, const char *argv[])
+{
+
+    if (argc != 2)
+    {
+        printf("Usage: ./exec matrix_file");
+        return 0;
+    }
+
+    int *row_ptr, *col_ind, num_rows, num_cols, num_vals;
+    float *values;
+    float *matrixDiagonal;
+
+    const char *filename = argv[1];
+
+    double start_cpu, end_cpu, start_gpu, end_gpu;
+
+    read_matrix(&row_ptr, &col_ind, &values, &matrixDiagonal, filename, &num_rows, &num_cols, &num_vals);
+
+    int size = num_rows * sizeof(float);
+
+    float *x = (float *)malloc(size);
+
+    // Memory for gpu result
+    float *x_fromDevice = cudaMallocManaged(size * sizeof(float));
+
+    // Generate a random vector
+    srand(time(NULL));
+    for (int i = 0; i < num_rows; i++)
+    {
+        x[i] = (rand() % 100) / (rand() % 100 + 1); // the number we use to divide cannot be 0, that's the reason of the +1
+        x_fromDevice[i] = x[i];
+    }
+
+    // Compute in sw
+    start_cpu = get_time();
+    symgs_csr_sw(row_ptr, col_ind, values, num_rows, x, matrixDiagonal);
+    end_cpu = get_time();
+
+    // Compute in parallel on gpu 
+    start_gpu = get_time();
+    //parallel_symgs_csr_sw<<<num_blocks, threads_per_block>>>(row_ptr, col_ind, values, num_rows, x, matrixDiagonal);
+    end_gpu = get_time();
+
+    // Check GPU result against CPU
+    // if(isResultValid(x, x_fromDevice, size)) printf("Il risultato è valido.\n");
+    // else printf("Il risultato non è valido.\n");
+    
+    // Print time
+    printf("SYMGS Time CPU: %.10lf\n", end_cpu - start_cpu);
+    // printf("SYMGS Time GPU: %.10lf\n", end_gpu - start_gpu);
+
+    // Free
+    cudaFree(row_ptr);
+    cudaFree(col_ind);
+    cudaFree(values);
+    cudaFree(matrixDiagonal);
+    free(x);
+    cudaFree(x_fromDevice);
+    return 0;
+}
